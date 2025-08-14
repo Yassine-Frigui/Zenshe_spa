77ab84efa3e9c840f5b008a747d42dca571dfef2
Youssef_Frigui <youssef.frigui13@gmail.com>
2025-08-14 16:41:41 +0100

    please work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Only used in development
+        target: 'https://zenshe-spa.onrender.com/', // Only used in development
         changeOrigin: true,
         secure: false,
       }
